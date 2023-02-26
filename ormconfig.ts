89ea3884a2542c3dd75f91875cef601a13aef1c3
vkin <victorrealm@gmail.com>
2023-02-26 13:19:54 -0300

    refactor: update typeorm config file

---
+++ ormconfig.ts
@@ -0,0 +1,20 @@
+import { DataSource } from "typeorm"
+import { Game } from "./src/modules/games/entities/Game"
+import { User } from "./src/modules/users/entities/User"
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'queries_challenge',
+  synchronize: false,
+  logging: false,
+  entities: [
+      Game,
+      User
+  ],
+  subscribers: [],
+  migrations: ['./src/database/migrations/*.ts'],
+});
